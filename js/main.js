95039a79ad3d6c22f13f59f4ab532447264ed617
homerioni <homerion13@gmail.com>
2022-02-25 19:39:28 +0300

    create section text-reviews

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 'use strict';
 
 $(document).ready(function () {
-    $('.video-reviews__item-rating').each(function () {
+    // Настройка оценок для отзывов
+    $('.review-rating').each(function () {
         let rating = $(this).attr('rating');
         if (rating <= 2) {
-            $(this).find('.video-reviews__item-rating-svg:nth-child(-n+' + rating + ')').addClass('orange');
-            $(this).parent().find('.video-reviews__item-rating-text').text('Отрицательный отзыв');
+            $(this).find('.review-rating__svg:nth-child(-n+' + rating + ')').addClass('orange');
+            $(this).parent().find('.review-rating__text').text('Отрицательный отзыв');
         } else if (rating == 3) {
-            $(this).find('.video-reviews__item-rating-svg:nth-child(-n+' + rating + ')').addClass('orange');
-            $(this).parent().find('.video-reviews__item-rating-text').text('Нейтральный отзыв');
+            $(this).find('.review-rating__svg:nth-child(-n+' + rating + ')').addClass('orange');
+            $(this).parent().find('.review-rating__text').text('Нейтральный отзыв');
         } else {
-            $(this).find('.video-reviews__item-rating-svg:nth-child(-n+' + rating + ')').addClass('green');
-            $(this).parent().find('.video-reviews__item-rating-text').text('Положительный отзыв');
+            $(this).find('.review-rating__svg:nth-child(-n+' + rating + ')').addClass('green');
+            $(this).parent().find('.review-rating__text').text('Положительный отзыв');
         }
     });
 });
